9dd6824ff9c7fc08982e02c6f79d25f2a10084a7
hevmag <hevmag@Hevs-MacBook-Pro.local>
2023-08-05 15:25:24 +0100

    add dialogue slider to BoxHeading with next button functionality

--- src/lib/components/molecules/BoxHeadingWrapper.svelte
+++ src/lib/components/molecules/BoxHeadingWrapper.svelte
@@ -1,3 +1,3 @@
-<div class={`w-3/4 h-fit p-5 bg-orange-100 border-8 border-slate-900`}>
+<div class={`relative w-3/4 max-w-3xl h-52 p-5 bg-orange-100 border-8 border-slate-900`}>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,43 @@
 <script>
 	import { BoxHeadingWrapper } from "$lib/index.js";
+	import { slide } from "svelte/transition";
+
+	let dialogueOptions = [
+		"Hey there!", 
+        "My name is Hev.",
+		"I am a Software Developer and Tech Educator.",
+	];
+	let index = 0;
+
+	function nextDialogue() {
+		if (index === dialogueOptions.length - 1) index = 0;
+		else index++;
+	}
 </script>
 
-<body>
-	<header class={`w-full pt-5 grid grid-cols-1 justify-items-center`}>
+<main class="h-screen grid grid-rows-3">
+	<header
+		class={`row-span-1 w-full grid grid-cols-1 justify-items-center content-center`}
+	>
 		<BoxHeadingWrapper>
-			<h1 class="text-4xl font-game text-slate-900">
-				Hey there! <br />My name is Hev.
-			</h1>
+			{#key index}
+				<h1 transition:slide class="text-4xl font-game text-slate-900">
+					{dialogueOptions[index]}
+				</h1>
+			{/key}
+			<button
+				class={`absolute bottom-4 right-4 text-4xl transition ease-in-out hover:text-amber-500`}
+				on:click={nextDialogue}>{index === dialogueOptions.length - 1 ? "◀" : "▶"}</button
+			>
 		</BoxHeadingWrapper>
 	</header>
-</body>
+	<section
+		class={`row-span-2 grid grid-cols-1 justify-items-center content-center`}
+	>
+		<img
+			class={`scale-50`}
+			src={`../../images/hevhi.gif`}
+			alt="pixel art gif of a small hev waving"
+		/>
+	</section>
+</main>
