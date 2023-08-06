d08bbc46f79ccbcfd6d88538f365159c8f4c6fa7
hevmag <hevmag@Hevs-MacBook-Pro.local>
2023-08-06 10:18:22 +0100

    add responsive sizing to home elements

--- src/lib/components/molecules/BoxHeadingWrapper.svelte
+++ src/lib/components/molecules/BoxHeadingWrapper.svelte
@@ -1,3 +1,3 @@
-<div class={`relative w-3/4 max-w-3xl h-52 p-5 bg-orange-100 border-8 border-slate-900`}>
+<div class={`relative w-3/4 max-w-3xl h-52 p-5 bg-sky-100 border-8 border-slate-900`}>
 	<slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,11 @@
 	import { BoxHeadingWrapper } from "$lib/index.js";
 	import { slide } from "svelte/transition";
 
-	let dialogueOptions = [
+	const dialogueOptions = [
 		"Hey there!", 
         "My name is Hev.",
 		"I am a Software Developer and Tech Educator.",
+        "Feel free to take a look around..."
 	];
 	let index = 0;
 
@@ -15,27 +16,27 @@
 	}
 </script>
 
-<main class="h-screen grid grid-rows-3">
+<main class="bg-slate-100 w-full h-screen box-border grid grid-rows-3">
 	<header
 		class={`row-span-1 w-full grid grid-cols-1 justify-items-center content-center`}
 	>
 		<BoxHeadingWrapper>
 			{#key index}
-				<h1 transition:slide class="text-4xl font-game text-slate-900">
+				<h1 transition:slide class={`text-4xl sm:text-5xl md:text-5xl lg:text-6xl xl:text-6xl 2xl:text-6xl font-game text-slate-900`}>
 					{dialogueOptions[index]}
 				</h1>
 			{/key}
 			<button
-				class={`absolute bottom-4 right-4 text-4xl transition ease-in-out hover:text-amber-500`}
+				class={`absolute bottom-4 right-4 text-4xl transition ease-in-out hover:text-sky-500`}
 				on:click={nextDialogue}>{index === dialogueOptions.length - 1 ? "◀" : "▶"}</button
 			>
 		</BoxHeadingWrapper>
 	</header>
 	<section
-		class={`row-span-2 grid grid-cols-1 justify-items-center content-center`}
+		class={`row-span-2 grid justify-items-center content-center`}
 	>
 		<img
-			class={`scale-50`}
+			class={`p-5 scale-70 max-w-sm object-contain overflow-hidden`}
 			src={`../../images/hevhi.gif`}
 			alt="pixel art gif of a small hev waving"
 		/>
