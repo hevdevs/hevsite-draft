1f29eb4dd7e54c7f3e58742045d8ecc28e898296
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-06 18:29:07 +0100

    add initial logic and styles for nav

--- src/lib/components/molecules/DialogueBoxHeadingWrapper.svelte
+++ src/lib/components/molecules/DialogueBoxHeadingWrapper.svelte
@@ -1,6 +1,7 @@
 <script>
 	import { DialogueText, NextArrowButton } from "$lib/index";
-
+	export let selectPage;
+	export let pages;
 	const dialogueOptions = [
 		"Hey there!",
 		"My name is Hev.",
@@ -9,7 +10,10 @@
 	];
 
 	let index = 0;
-
+	let isOpen = false
+	function setOpen() {
+		isOpen = !isOpen
+	}
 	function getNextDialogue() {
 		if (index === dialogueOptions.length - 1) index = 0;
 		else index++;
@@ -22,6 +26,25 @@
 	{#key dialogueOptions[index]}
 		<DialogueText currDialogue={dialogueOptions[index]} />
 	{/key}
+	<div
+		role="button"
+		tabindex="0"
+		on:keydown={() => setOpen()}
+		class="absolute space-y-1 top-4 right-4"
+		on:click={() => setOpen()}
+	>
+		<span class="block w-8 h-1 bg-slate-900" />
+		<span class="block w-8 h-1 bg-slate-900" />
+		<span class="block w-8 h-1 bg-slate-900" />
+	</div>
+
+	{#if isOpen}
+		{#each pages as page}
+			<button class="menu-item" on:click={() => selectPage(page)}
+				>{page.name}</button
+			>
+		{/each}
+	{/if}
 	<NextArrowButton
 		{getNextDialogue}
 		isLast={index === dialogueOptions.length - 1}
