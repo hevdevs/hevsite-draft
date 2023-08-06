4648234786f458637c387f4705e2ace3b129ffc6
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-06 18:28:37 +0100

    add pages to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,6 @@ export { default as DialogueText } from "./components/atoms/DialogueText.svelte"
 export { default as NextArrowButton } from "./components/atoms/NextArrowButton.svelte";
 
 export { default as DialogueBoxHeadingWrapper } from "./components/molecules/DialogueBoxHeadingWrapper.svelte";
+
+export { default as Home } from "./components/pages/Home.svelte";
+export { default as Projects } from "./components/pages/Projects.svelte";
