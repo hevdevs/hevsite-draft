8e0895d5be8c884b232a5d76cd1ed85e81d5559a
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-24 19:14:20 +0100

    add consistent container for content

--- src/lib/components/pages/Projects.svelte
+++ src/lib/components/pages/Projects.svelte
@@ -1,3 +1,7 @@
-<section class={`row-span-2 grid justify-items-center content-center`}>
-	<h2>hellooooo projects</h2>
+<section class={`row-span-2 grid h-full justify-items-center p-0`}>
+	<section
+		class={`w-3/4 max-w-3xl h-[60vh] bg-sky-100 border-8 border-slate-900 grid justify-items-center content-center`}
+	>
+		<h2>hellooooo projects</h2>
+	</section>
 </section>
--- src/lib/components/pages/Socials.svelte
+++ src/lib/components/pages/Socials.svelte
@@ -1,3 +1,7 @@
-<section class={`row-span-2 grid justify-items-center content-center`}>
-	<h2>hellooooo socials</h2>
+<section class={`row-span-2 grid justify-items-center`}>
+	<section
+		class={`w-3/4 max-w-3xl h-[60vh] bg-sky-100 border-8 border-slate-900 grid justify-items-center content-center`}
+	>
+		<h2>hellooooo socials</h2>
+	</section>
 </section>
