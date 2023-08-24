d90b29c816a2e7172debc429106cb7132a685b5a
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-24 19:12:46 +0100

    add dialogue options functionality for each page

--- src/lib/components/molecules/DialogueBoxHeadingWrapper.svelte
+++ src/lib/components/molecules/DialogueBoxHeadingWrapper.svelte
@@ -2,13 +2,28 @@
 	import { DialogueText, NextArrowButton } from "$lib/index";
 	export let selectPage;
 	export let pages;
-	export let currPage; 
-	const dialogueOptions = [
-		"Hey there!",
-		"My name is Hev.",
-		"I am a Software Developer and Tech Educator.",
-		"Feel free to take a look around...",
-	];
+	export let currPage;
+	const dialogueOptions = {
+		home: [
+			"Hey there!",
+			"My name is Hev.",
+			"I am a Software Developer and Tech Educator.",
+			"Feel free to take a look around.",
+		],
+		projects: [
+			"Here is some of my recent dev work...",
+			"I'm always looking for new challenges to level up.",
+		],
+		skills: [
+			"Check out the skills that pay the bills...",
+			"I also know a tonne of dad jokes.",
+			"You might say I'm the whole package...",
+		],
+		socials: [
+			"Get in touch with me!",
+			"I may be open to collaborate on cool projects...",
+		],
+	};
 
 	let index = 0;
 	let isOpen = false;
@@ -16,7 +31,7 @@
 		isOpen = !isOpen;
 	}
 	function getNextDialogue() {
-		if (index === dialogueOptions.length - 1) index = 0;
+		if (index === dialogueOptions[currPage.name].length - 1) index = 0;
 		else index++;
 	}
 </script>
@@ -24,8 +39,8 @@
 <div
 	class={`relative w-3/4 max-w-3xl h-52 p-5 bg-sky-100 border-8 border-slate-900`}
 >
-	{#key dialogueOptions[index]}
-		<DialogueText currDialogue={dialogueOptions[index]} />
+	{#key dialogueOptions[currPage.name][index]}
+		<DialogueText currDialogue={dialogueOptions[currPage.name][index]} />
 	{/key}
 	<div
 		role="button"
@@ -40,14 +55,21 @@
 	</div>
 
 	{#if isOpen}
-		<div class={`w-1/3 min-w-fit z-10 p-2.5 top-12 -right-2 absolute bg-sky-500 border-8 border-slate-900 text-justify`}>
+		<div
+			class={`w-1/3 min-w-fit z-10 p-2.5 top-12 -right-2 absolute bg-sky-500 border-8 border-slate-900 text-justify`}
+		>
 			{#each pages as page}
 				<button
-					class={`text-3xl menu-item hover:text-slate-100 font-game leading-relaxed ${currPage.name === page.name? 'underline underline-offset-8 decoration-solid decoration-4' : ''}`}
+					class={`text-3xl menu-item hover:text-slate-100 font-game leading-relaxed ${
+						currPage.name === page.name
+							? "underline underline-offset-8 decoration-solid decoration-4"
+							: ""
+					}`}
 					role="link"
 					on:click={() => {
 						setOpen();
 						selectPage(page);
+						index = 0;
 					}}>{page.name}</button
 				>
 				<br />
@@ -56,6 +78,6 @@
 	{/if}
 	<NextArrowButton
 		{getNextDialogue}
-		isLast={index === dialogueOptions.length - 1}
+		isLast={index === dialogueOptions[currPage.name].length - 1}
 	/>
 </div>
