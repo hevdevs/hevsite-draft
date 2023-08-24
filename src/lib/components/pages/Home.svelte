002a5c9d0a1227391930278da9bc7f1b37b1339e
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-24 19:13:17 +0100

    add bio block

--- src/lib/components/pages/Home.svelte
+++ src/lib/components/pages/Home.svelte
@@ -1,7 +1,21 @@
-<section class={`row-span-2 grid justify-items-center content-center`}>
-		<img
-			class={`p-5 scale-70 max-w-sm object-contain overflow-hidden`}
-			src={`../../images/hevhi.gif`}
-			alt="pixel art gif of a small hev waving"
-		/>
+<section class={`row-span-2 grid justify-items-center`}>
+	<img
+		class={`p-5 relative scale-70 max-w-sm object-contain overflow-hidden z-10`}
+		src={`../../images/hevhi.gif`}
+		alt="pixel art gif of a small hev waving"
+	/>
+	<div
+		class={`absolute bottom-20 right-5 max-w-fit p-2.5 bg-sky-500 border-8 border-slate-900`}
+	>
+		<h2 class={`text-3xl font-game text-slate-900`}>Job Title:</h2>
+		<p class={`text-2xl font-game text-slate-900`}>
+			Software Engineer and Seminar Lead at Northcoders.
+		</p>
+		<h2 class={`text-3xl font-game text-slate-900`}>Location:</h2>
+		<p class={`text-2xl font-game text-slate-900`}>Manchester, UK.</p>
+		<p class={`text-3xl font-game text-slate-900`}>Likes:</p>
+		<p class={`text-2xl font-game text-slate-900`}>
+			Music, dogs, coffee, video games.
+		</p>
+	</div>
 </section>
