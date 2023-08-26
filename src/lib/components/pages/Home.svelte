00cd409bcdc19aa150d8aad6073e21564ef69862
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-26 21:21:37 +0100

    center bio

--- src/lib/components/pages/Home.svelte
+++ src/lib/components/pages/Home.svelte
@@ -5,16 +5,28 @@
 		alt="pixel art gif of a small hev waving"
 	/>
 	<div
-		class={`absolute bottom-20 right-5 max-w-fit p-2.5 bg-sky-500 border-8 border-slate-900`}
+		class={`absolute bottom-10 justify-self-center max-w-fit p-2.5 mt-5 m-2.5 bg-sky-500 border-8 border-slate-900`}
 	>
-		<h2 class={`text-3xl font-game text-slate-900`}>Job Title:</h2>
-		<p class={`text-2xl font-game text-slate-900`}>
+		<h2
+			class={`text-3xl underline underline-offset-4 decoration-solid decoration-4 font-game text-slate-900`}
+		>
+			Job Title:
+		</h2>
+		<p class={`text-3xl font-game text-slate-900`}>
 			Software Engineer and Seminar Lead at Northcoders.
 		</p>
-		<h2 class={`text-3xl font-game text-slate-900`}>Location:</h2>
-		<p class={`text-2xl font-game text-slate-900`}>Manchester, UK.</p>
-		<p class={`text-3xl font-game text-slate-900`}>Likes:</p>
-		<p class={`text-2xl font-game text-slate-900`}>
+		<h2
+			class={`text-3xl underline underline-offset-4 decoration-solid decoration-4 font-game text-slate-900`}
+		>
+			Location:
+		</h2>
+		<p class={`text-3xl font-game text-slate-900`}>Manchester, UK.</p>
+		<p
+			class={`text-3xl underline underline-offset-4 decoration-solid decoration-4 font-game text-slate-900`}
+		>
+			Likes:
+		</p>
+		<p class={`text-3xl font-game text-slate-900`}>
 			Music, dogs, coffee, video games.
 		</p>
 	</div>
