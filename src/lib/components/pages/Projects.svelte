45d90e81f9c0a248ea747784425adb1077338ff1
hevdevs <hevmag@Hevs-MacBook-Pro.local>
2023-08-27 15:05:46 +0100

    add scrollable element with flex col

--- src/lib/components/pages/Projects.svelte
+++ src/lib/components/pages/Projects.svelte
@@ -1,7 +1,15 @@
 <section class={`row-span-2 grid h-full justify-items-center p-0`}>
 	<section
-		class={`w-3/4 max-w-3xl h-[60vh] bg-sky-100 border-8 border-slate-900 grid justify-items-center content-center`}
+		class={`w-3/4 max-w-3xl h-[60vh] bg-sky-100 border-8 border-slate-900 flex-col space-y-6 p-5 justify-items-center content-center items-center overflow-y-scroll snap-y snap-mandatory`}
 	>
-		<h2>hellooooo projects</h2>
+		<div
+			class={`snap-always snap-center w-full max-h-full aspect-square bg-sky-300 border-8 border-slate-900 rounded-lg`}
+		/>
+		<div
+			class={`snap-always snap-center w-full max-h-full aspect-square bg-sky-300 border-8 border-slate-900 rounded-lg`}
+		/>
+		<div
+			class={`snap-always snap-center w-full max-h-full aspect-square bg-sky-300 border-8 border-slate-900 rounded-lg`}
+		/>
 	</section>
 </section>
